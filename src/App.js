45f1b1178fa4050cb7898b0240d4ca8d99f43483
João <santoscostaj808@gmail.com>
2022-03-10 17:00:01 -0300

    Requisito 03

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+// Requisito realizados por todos do grupo:
+// João Vitor Santos Costa, Maria Clara Medeiros Paulino,
+// Rafael de Alvarenga Reis, Angelica Diniz, Alexsandro Pinheiro Ferreira.
+
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 import Search from './pages/Search';
+import Cart from './pages/Cart';
 
 class App extends React.Component {
   render() {
@@ -10,7 +15,13 @@ class App extends React.Component {
         <BrowserRouter>
           <Switch>
             <Route exact path="/" component={ Search } />
+            <Route path="/carrinho" component={ Cart } />
           </Switch>
+          <button type="button">
+            <Link to="/carrinho" data-testid="shopping-cart-button">
+              carrinho de compras
+            </Link>
+          </button>
         </BrowserRouter>
       </div>
     );
--- src/componentes/ButtonRadios.js
+++ src/componentes/ButtonRadios.js
@@ -4,17 +4,14 @@
 
 import React, { Component } from 'react';
 
-class Search extends Component {
+class ButtonRadio extends Component {
   render() {
     return (
       <div>
-        <input type="text" name="name" />
-        <p data-testid="home-initial-message">
-          Digite algum termo de pesquisa ou escolha uma categoria.
-        </p>
+        <button />
       </div>
     );
   }
 }
 
-export default Search;
+export default ButtonRadio;
