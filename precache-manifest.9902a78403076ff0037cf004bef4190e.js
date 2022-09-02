a93e2cf8e3586d690358f1d3a780bb79a5583b17
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2022-09-02 16:25:29 -0300

    Updates

---
+++ precache-manifest.9902a78403076ff0037cf004bef4190e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e8e3c2ab1aa3df8ccf434bcdf5f954b",
+    "url": "/online-store/index.html"
+  },
+  {
+    "revision": "9088fc877b4044462081",
+    "url": "/online-store/static/css/main.8af4bfcc.chunk.css"
+  },
+  {
+    "revision": "397a3222ad4875d7663f",
+    "url": "/online-store/static/js/2.92ed310e.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/online-store/static/js/2.92ed310e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9088fc877b4044462081",
+    "url": "/online-store/static/js/main.a8ec1019.chunk.js"
+  },
+  {
+    "revision": "ff4896c2419cfc1775c2",
+    "url": "/online-store/static/js/runtime-main.a451c0e5.js"
+  }
+]);
